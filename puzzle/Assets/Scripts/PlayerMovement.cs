daa53d592a0e644567730ee9e47da4bae4e08056
ems <emsgao2004@gmail.com>
2025-10-18 18:31:45 -0400

    layout2
    - level 2 layout
    - door assets

--- puzzle/Assets/Scripts/PlayerMovement.cs
+++ puzzle/Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,7 @@ public class PlayerMovement : MonoBehaviour
 {
     private float horizontal;
     private float speed = 8f;
-    private float jumpingPower = 16f;
+    public float jumpingPower = 16f;
     private bool isFacingRight = true;
 
     private float coyoteTime = 0.2f;
